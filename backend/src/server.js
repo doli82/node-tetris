eb7e71cfdb4885faa1424b4be7bd8c7e4004b455
doli82 <39415174+doli82@users.noreply.github.com>
2020-05-01 01:40:43 -0300

    Configura server para hospedagem no heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,8 @@
 const GameServer = require('./game-server');
 
+<<<<<<< HEAD
 const gameserver = new GameServer(process.event.port || 3300);
+=======
+const gameserver = new GameServer(process.event.PORT || 3300);
+>>>>>>> Configura server para hospedagem no heroku
 gameserver.run();
